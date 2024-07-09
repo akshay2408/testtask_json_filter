fc3c66c4c921cde34254d857ed786f6fdf7ca641
akshay2408 <akshaybabar2408@gmail.com>
2024-07-09 16:15:32 +0530

    initial commit

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::UsersController < ApplicationController
+	# app/controllers/api/v1/users_controller.rb
+	def index
+		@users = User.all
+		render json: @users
+	end
+
+	def create
+		debugger
+  	@user = User.new(user_params)
+  	if @user.save
+    		render json: @user, status: :created
+  	else
+    		render json: @user.errors, status: :unprocessable_entity
+  	end
+	end
+
+  def filter
+  	# debugger
+  	campaign_names = params[:campaign_names].split(',')
+  	@users = User.where("JSON_CONTAINS(campaigns_list, ?) > 0", campaign_names.map { |name| { campaign_name: name } }.to_json)
+  	render json: @users
+  end	
+
+	private
+
+	def user_params
+    	params.require(:user).permit(:name, :email, :campaigns_list)
+	end
+end
